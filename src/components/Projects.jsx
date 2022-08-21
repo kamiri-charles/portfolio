a75125a67c381bfca91ed08863bde22f9befd081
Charles Kamiri <charleskamiri6@gmail.com>
2022-08-21 20:13:03 +0300

    Minor UI updates.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import { faPython, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faGamepad, faInfinity } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft,  faGamepad, faInfinity } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/Projects.scss';
 import { useEffect } from 'react';
@@ -10,18 +10,29 @@ import load_projects from '../assets/scripts/projects';
 
 const Projects = () => {
 
+
+    /* Open and close projects menu function for smaller devices */
+    const projects_nav_control = () => {
+        const projects_nav = document.getElementById('projects-nav');
+
+
+        !projects_nav.classList.contains('inactive') ? projects_nav.classList.add('inactive') : projects_nav.classList.remove('inactive');
+
+        
+    }
+
     useEffect(() => {
-        const works = document.getElementById('works');
+        const works_container = document.getElementById('works-container');
         const projects_arr = [
             {
                 'name': 'react',
                 'el': document.getElementById('1'),
-                'total_projects': 4
+                'total_projects': 10
             },
             {
                 'name': 'python',
                 'el': document.getElementById('2'),
-                'total_projects': 2
+                'total_projects': 1
             },
             {
                 'name': 'game-dev',
@@ -31,18 +42,19 @@ const Projects = () => {
             {
                 'name': 'misc',
                 'el': document.getElementById('4'),
-                'total_projects': 2
+                'total_projects': 1
             }
         ];
-        load_projects(works, projects_arr[0].total_projects);
+        load_projects(works_container, projects_arr[0].total_projects);
 
         projects_arr.forEach(obj => {
             obj.el.addEventListener('click', () => {
+                window.innerWidth <= 753 ? projects_nav_control() : console.log('on a laptop');
                 for (let i = 0; i < projects_arr.length; i++) {
                     projects_arr[i].el.classList.remove('active');
                 }
                 obj.el.classList.add('active');
-                load_projects(works, obj.total_projects);
+                load_projects(works_container, obj.total_projects);
             });
         })
     });
@@ -50,7 +62,13 @@ const Projects = () => {
 
     return (
         <div id="projects">
-            <div className="projects-nav">
+
+            <div className="projects-nav-control-btn" id='projects-nav-control-btn' onClick={ projects_nav_control }>
+                <FontAwesomeIcon icon={ faAngleLeft } />
+            </div>
+
+            <div className="projects-nav" id='projects-nav'>
+
 
                 {/* ReactJs */}
                 <div className="nav-item active" id='1'>
@@ -79,6 +97,9 @@ const Projects = () => {
 
 
             <div className="works" id='works'>
+                <div className="works-container" id='works-container'>
+
+                </div>
             </div>
         </div>
     )
