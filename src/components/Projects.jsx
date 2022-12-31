d541b5865e5442d0d027359a6415d514fa0eca40
Charles Kamiri <charleskamiri6@gmail.com>
2022-12-31 03:10:49 +0300

    setup custom api proxy server

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
         setProjects(null);
         setTimeout(() => {
             (async () => {
-                fetch(`https://projects-api.herokuapp.com/api/projects/${active}/`)
+                fetch(`/api/projects/${active}/`)
                 .then(res => res.json())
                 .then(data => setProjects(data))
             }) ();
---
+++ src/setupProxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://projects-api.herokuapp.com',
+            changeOrigin: true,
+        }),
+        
+        '/images',
+        createProxyMiddleware({
+            target: 'https://projects-api.herokuapp.com',
+            changeOrigin: true,
+        })
+        );
+    };
