db8bf6b885008916c4e37aff2210446b7fa81f97
Charles Kamiri <charleskamiri6@gmail.com>
2022-12-31 03:51:17 +0300

    project image not loading fix

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ const Project = ({title, desc, image, link}) => {
     return (
         <a className="project" href={link} target="_blank" rel="noreferrer">
             <div className="title">{title}</div>
-            <img src={image} alt="project_image" />
+            <img src={"http://projects-api.herokuapp.com/" + image} alt="project_image" />
             <div className="desc">{desc}</div>
         </a>
     )
