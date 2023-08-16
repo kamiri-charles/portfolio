d24a3966496e82c2f1c8c316c87eb089104d56cb
Charles Kamiri <charleskamiri6@gmail.com>
2023-08-16 16:00:54 +0300

    Fixed whatsapp link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,23 +19,23 @@ const Contact = () => {
     });
 
     return (
-        <div id="contact">
-            <div className="sub-title">Get in touch</div>
-            <div className="left">
-                <form action="https://formsubmit.co/5468185aaec8dc6707c6f8bc9216c88c" method='POST'>
-                    <div className="sub-sub-title">Send me an email.</div>
+        <div id='contact'>
+            <div className='sub-title'>Get in touch</div>
+            <div className='left'>
+                <form action='https://formsubmit.co/5468185aaec8dc6707c6f8bc9216c88c' method='POST'>
+                    <div className='sub-sub-title'>Send me an email.</div>
                     <label>
                         Subject
-                        <input type="text" id='input_1' name='Subject' required />
+                        <input type='text' id='input_1' name='Subject' required />
                     </label>
 
                     <label>
                         Email
-                        <input type="email" id='input_2' name='Email' required />
+                        <input type='email' id='input_2' name='Email' required />
                     </label>
-                    <input type="hidden" name="_captcha" value="false" />
-                    <input type="hidden" name="_next" value="https://portfolio.kamiri.live/#/email-submission-success" />
-                    <div className="to">
+                    <input type='hidden' name='_captcha' value='false' />
+                    <input type='hidden' name='_next' value='https://portfolio.kamiri.live/#/email-submission-success' />
+                    <div className='to'>
                         <span>To: </span>
                         charleskamiri6@gmail.com
                         </div>
@@ -43,24 +43,24 @@ const Contact = () => {
                     <button className='send'>Send</button>
                 </form>
             </div>
-            <div className="border"></div>
+            <div className='border'></div>
 
 
-            <div className="right">
+            <div className='right'>
                 {/* Github */}
-                <a href="https://github.com/kamiri-charles" className='github' target='_blank' rel='noreferrer'>
-                    <div className="icon">
+                <a href='https://github.com/kamiri-charles' className='github' target='_blank' rel='noreferrer'>
+                    <div className='icon'>
                         <FontAwesomeIcon icon={ faGithub } />
                     </div>
-                    <div className="link-text">Contribute to some of my projects on GitHub <br /> @kamiri-charles</div>
+                    <div className='link-text'>Contribute to some of my projects on GitHub <br /> @kamiri-charles</div>
                 </a>
 
                 {/* Whatsapp */}
-                <a href="" className='whatsapp' rel='noreferrer'>
-                    <div className="icon">
+                <a href='https://wa.me/254797317704' className='whatsapp' target='_blank' rel='noreferrer'>
+                    <div className='icon'>
                         <FontAwesomeIcon icon={ faWhatsapp } />
                     </div>
-                    <div className="link-text">+254 797-317-704 <br />Send a quick text, maybe even a call... and let's connect</div>
+                    <div className='link-text'>+254 797-317-704 <br />Send a quick text, maybe even a call... and let's connect</div>
                 </a>
             </div>
         </div>
