29be48eb63803f8057e5d3787d5f783d1bd2bcbd
Charles Kamiri <charleskamiri6@gmail.com>
2023-09-14 21:14:27 +0300

    Routing bug fix trial-1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import "./App.scss";
 const App = () => {
   return (
     <div className='App'>
-      <Router basename="portfolio">
+      <Router basename="/portfolio">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/home" element={<Home />} />
