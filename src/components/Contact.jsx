5bf898dcc3469113d28e6730438800656c6548d8
Charles Kamiri <charleskamiri6@gmail.com>
2023-10-11 14:46:54 +0300

    Minor updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,59 +17,87 @@ const Contact = () => {
     });
 
     return (
-        <div id='contact'>
-            <div className='sub-title'>Get in touch</div>
-            <div className='left'>
-                <form action='https://formsubmit.co/5468185aaec8dc6707c6f8bc9216c88c' method='POST'>
-                    <label>
-                        Subject
-                        <input type='text' ref={input_1} name='Subject' required />
-                    </label>
+      <div id="contact">
+        <div className="sub-title">Get in touch</div>
+        <div className="left">
+          <form
+            action="https://formsubmit.co/5468185aaec8dc6707c6f8bc9216c88c"
+            method="POST"
+          >
+            <label>
+              Subject
+              <input type="text" ref={input_1} name="Subject" required />
+            </label>
 
-                    <label>
-                        Email
-                        <input type='email' ref={input_2} name='Email' required />
-                    </label>
-                    <input type='hidden' name='_captcha' value='false' />
-                    <input type='hidden' name='_next' value='https://portfolio.kamiri.live/portfolio/email-submission-success' />
-                    <div className='to'>
-                        <span>To: </span>
-                        charleskamiri6@gmail.com
-                        </div>
-                    <textarea placeholder='Message'></textarea>
-                    <button className='send'>Send</button>
-                </form>
+            <label>
+              Email
+              <input type="email" ref={input_2} name="Email" required />
+            </label>
+            <input type="hidden" name="_captcha" value="false" />
+            <input
+              type="hidden"
+              name="_next"
+              value="https://portfolio.kamiri.live/portfolio/email-submission-success"
+            />
+            <div className="to">
+              <span>To: </span>
+              charleskamiri6@gmail.com
             </div>
-            <div className='border'></div>
-
-
-            <div className='right'>
-                {/* Github */}
-                <a href='https://github.com/kamiri-charles' className='github' target='_blank' rel='noreferrer'>
-                    <div className='icon'>
-                        <i className="bx bxl-github"></i>
-                    </div>
-                    <div className='link-text'>Contribute to some of my projects on GitHub <br /> @kamiri-charles</div>
-                </a>
-
-                {/* Discord */}
-                <a href='https://discord.com' className='discord' target='_blank' rel='noreferrer'>
-                    <div className='icon'>
-                        <i className="bx bxl-discord-alt"></i>
-                    </div>
-                    <div className='link-text'>I'm also available on Discord. <br /> @kamiri_ke</div>
-                </a>
-
-                {/* Whatsapp */}
-                <a href='https://wa.me/254797317704' className='whatsapp' target='_blank' rel='noreferrer'>
-                    <div className='icon'>
-                        <i className="bx bxl-whatsapp"></i>
-                    </div>
-                    <div className='link-text'>+254 797-317-704 <br />Send a quick text, maybe even a call... and let's connect</div>
-                </a>
-            </div>
+            <textarea placeholder="Message"></textarea>
+            <button className="send">Send</button>
+          </form>
         </div>
-    )
+        <div className="border"></div>
+
+        <div className="right">
+          {/* Github */}
+          <a
+            href="https://github.com/kamiri-charles"
+            className="github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon">
+              <i className="bx bxl-github"></i>
+            </div>
+            <div className="link-text">
+              Contribute to some of my projects on GitHub <br /> @kamiri-charles
+            </div>
+          </a>
+
+          {/* Discord */}
+          <a
+            href="https://discord.com"
+            className="discord"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon">
+              <i className="bx bxl-discord-alt"></i>
+            </div>
+            <div className="link-text">
+              I'm also available on Discord. <br /> @kamiri_ke
+            </div>
+          </a>
+
+          {/* Whatsapp */}
+          <a
+            href="https://wa.me/254797317704"
+            className="whatsapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon">
+              <i className="bx bxl-whatsapp"></i>
+            </div>
+            <div className="link-text">
+              +254 797-317-704 <br />
+              Send a quick text, maybe even a call... and let's connect
+            </div>
+          </a>
+        </div>
+      </div>
+    );
 }
 
 export default Contact;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.scss';
 
 const Navbar = () => {
-
+    
     let [menu_icon, setMenuIcon] = useState('bx bx-menu-alt-left');
-
+    
     const navbar = useRef();
     const menu = useRef();
-
+    
     let open_menu = () => {
         if (menu.current.classList.contains('active')) {
             menu.current.classList.remove('active');
@@ -17,62 +17,53 @@ const Navbar = () => {
             menu.current.classList.add('active');
             setMenuIcon('bx bx-x');
         }
-}
+    }
 
-
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            window.scrollY > 5 ? navbar.current.classList.add('scrolled') : navbar.current.classList.remove('scrolled');
-        })
-    })
     return (
         <div id="navbar" ref={navbar}>
             <div id="ham-menu" onClick={ open_menu }>
                 <i className={menu_icon}></i>
             </div>
-
+            
             <div className="navbar-left">
                 <Link to="/">
-                    <div className="name">
-                        Charles Kamiri
-                    </div>
+                    <div className="name">Charles Kamiri</div>
                 </Link>
             </div>
-
+            
             <div className="navbar-right">
                 {/* Projects */}
                 <Link to='/projects'>
                     <div className="link">Projects</div>
                 </Link>
-
+            
                 {/* Certification */}
                 <Link to='/certification'>
                     <div className="link">Certification</div>
                 </Link>
-
+            
                 {/* Contact */}
                 <Link to='/contact'>
                     <div className="link">Contact</div>
                 </Link>
-
             </div>
-
+            
             {/* Mobile navbar */}
             <div ref={menu} id='menu'>
                 <Link to='/projects'>
                     <div className="link" onClick={open_menu}>Projects</div>
                 </Link>
-
+            
                 <Link to='/certification'>
                     <div className="link" onClick={open_menu}>Certification</div>
                 </Link>
-
+            
                 <Link to='/contact'>
                     <div className="link" onClick={open_menu}>Contact</div>
                 </Link>
-
+            
             </div>
         </div>
-    )
-}
-export default Navbar;
+        )
+    }
+    export default Navbar;
